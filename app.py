2c9e552a6b4111eb6b1996060eb50feb53d9803e
maryPopplns <spencer.knightusn@gmail.com>
2022-05-24 19:09:59 -0700

    login route complete

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
-import time
-import hmac
-import flask
-from datetime import datetime
+import jwt
+from functools import wraps
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 from flask import Flask, jsonify, request, make_response
 from controllers import headlines_controller, coins_controller
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -27,12 +26,24 @@ db = SQLAlchemy(app)
 
 
 # user model
+# TODO change user model
 class User(db.Model):
     __table_name = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     date_registered = db.Column(db.DateTime, default=datetime.utcnow())
+
+
+def encode_token(user_id):
+    payload = {
+        'exp': datetime.utcnow() + timedelta(days=1),
+        'iat': datetime.utcnow(),
+        'sub': user_id
+    }
+    token = jwt.encode(payload, os.getenv('SECRET_KEY'),
+                       algorithm='HS256')
+    return token
 
 
 @app.route('/register', methods=['POST'])
@@ -68,6 +79,67 @@ def register_user():
             "message": "User already exists"
         }
         return make_response(jsonify(resp)), 202
+
+
+@app.route('/login', methods=['POST'])
+def post():
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password) == True:
+            auth_token = encode_token(user.id)
+            resp = {
+                "status": "success",
+                "message": "Successfully logged in",
+                'auth_token': auth_token
+            }
+            return make_response(jsonify(resp)), 200
+        else:
+            resp = {
+                "status": "Error",
+                "message": "User does not exist"
+            }
+            return make_response(jsonify(resp)), 404
+
+    except Exception as e:
+        print(e)
+        resp = {
+            "Status": "error",
+            "Message": "User login failed"
+        }
+        return make_response(jsonify(resp)), 404
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+        try:
+            data = jwt.decode(
+                token, os.getenv('SECRET_KEY'), algorithms=["HS256"])
+            current_user = User.query.filter_by(
+                id=data['sub']).first()
+        except:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(current_user, *args, **kwargs)
+    return decorator
+
+
+@app.route('/protected', methods=['GET'])
+@token_required
+def protected(f):
+    resp = {"message": "you are viewing a protected route"}
+    return make_response(jsonify(resp)), 404
 
 
 @app.route('/headlines', methods=['GET'])
